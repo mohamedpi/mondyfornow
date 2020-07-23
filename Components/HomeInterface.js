5a24b2999c6ad3ed5ac379be85f8cd0602fa1f4e
chebbi <mohamed.chebbi1@esprit.tn>
2020-07-23 06:03:24 +0100

    second commit

--- Components/HomeInterface.js
+++ Components/HomeInterface.js
@@ -1,14 +1,111 @@
 import React from "react"
-import {View,Text,Image} from "react-native"
+import {View,Text,Image,SafeAreaView,StyleSheet,ScrollView} from "react-native"
+import { Button, ThemeProvider,Card,ListItem} from 'react-native-elements';
+import { List } from 'react-native-paper';
 
 
 function HomeInterface(){
   return(
     <>
+    <SafeAreaView style ={styles.container} >
+      <ScrollView scrollEventThrottle={16}>
        <View>
-          <Text>welcome to home page</Text>
+          <Text style={styles.textStyle}>check our hot list</Text>
        </View>
+       <View>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} scrollEventThrottle={16}>
+            <Card
+               containerStyle={styles.cardStyle}
+               imageStyle={styles.imageStyle}
+               image={require('../assets/cover.jpg')}>
+             </Card>
+             <Card
+                containerStyle={styles.cardStyle}
+                imageStyle={styles.imageStyle}
+                image={require('../assets/clashofclans.jpg')}>
+              </Card>
+              <Card
+                 containerStyle={styles.cardStyle}
+                 imageStyle={styles.imageStyle}
+                 image={require('../assets/valorant.jpg')}>
+               </Card>
+               <Card
+                  containerStyle={styles.cardStyle}
+                  imageStyle={styles.imageStyle}
+                  image={require('../assets/uncharted4.jpg')}>
+                </Card>
+          </ScrollView>
+       </View>
+
+       <View>
+          <Text style={styles.textStyle}>check our new offers</Text>
+       </View>
+       <View>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} scrollEventThrottle={16}>
+            <Card
+               containerStyle={styles.cardStyle}
+               imageStyle={styles.imageStyle}
+               image={require('../assets/dimond1.png')}>
+             </Card>
+             <Card
+                containerStyle={styles.cardStyle}
+                imageStyle={styles.imageStyle}
+                image={require('../assets/dimond2.png')}>
+              </Card>
+              <Card
+                 containerStyle={styles.cardStyle}
+                 imageStyle={styles.imageStyle}
+                 image={require('../assets/dimond3.png')}>
+               </Card>
+               <Card
+                  containerStyle={styles.cardStyle}
+                  imageStyle={styles.imageStyle}
+                  image={require('../assets/dimond4.jpg')}>
+                </Card>
+          </ScrollView>
+
+          <View>
+             <Text style={styles.textStyle}>Discounts</Text>
+          </View>
+       </View>
+
+      </ScrollView>
+    </SafeAreaView>
     </>
   )
 }
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    padding:0,
+    backgroundColor:"#121419"
+  },
+  cardStyle:{
+    width:135,
+    height:135,
+    borderRadius:20
+
+  },
+  imageStyle:{
+    width:135,
+    height:135,
+    resizeMode:"cover",
+    borderRadius:20
+
+  },
+  titleStyle:{
+      flex:1,
+      width:135,
+      height:90,
+      marginBottom:120
+  },
+  textStyle:{
+    paddingHorizontal:15,
+    marginVertical:15,
+    fontFamily:"GlueGun-GW8Z",
+    fontSize:25,
+    color:"#dce0e9"
+  }
+
+})
 export default HomeInterface
--- Navigator/TabNavigator.js
+++ Navigator/TabNavigator.js
@@ -1,8 +1,9 @@
 import React from "react"
-import {View,Text} from "react-native"
+import {View,Text,SafeAreaView} from "react-native"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
 import HomeInterface from "../Components/HomeInterface"
 import CatalogInterface from "../Components/CatalogInterface"
@@ -10,32 +11,39 @@ import LikedInterface from "../Components/LikedInterface"
 import ProfileInterface from "../Components/ProfileInterface"
 import SearchInterface from "../Components/SearchInterface"
 
-const Tab = createBottomTabNavigator();
+const Tab =  createMaterialTopTabNavigator();
 
 function TabNavigator()
 {
   return (
     <>
+    <SafeAreaView style={{flex:1}}>
     <NavigationContainer>
       <Tab.Navigator
+      tabBarPosition="bottom"
       tabBarOptions={{
+      tabBarPosition:"bottom",
        activeTintColor: 'red',
-       TintColor:"white",
+       inactiveTintColor:"grey",
        showLabel:true,
-       style:{backgroundColor:"black"}
+       labelStyle:{fontSize:10,textTransform:"capitalize"},
+
+       style:{backgroundColor:"black"},
+       indicatorStyle:{height:0},
+       showIcon:true
      }}>
          <Tab.Screen name="Home" component={HomeInterface}
          options={{
           tabBarLabel: 'Home',
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
+            <MaterialCommunityIcons name="home" color={color} size={24} />
           ),
         }} />
 
          <Tab.Screen name="Catalog" component={CatalogInterface}  options={{
            tabBarLabel: 'Catalog',
            tabBarIcon: ({ color, size }) => (
-             <MaterialCommunityIcons name="details" color={color} size={size} />
+             <MaterialCommunityIcons name="details" color={color} size={24} />
            ),
          }}/>
 
@@ -43,7 +51,7 @@ function TabNavigator()
          options={{
           tabBarLabel: 'Liked',
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="heart" color={color} size={size} />
+            <MaterialCommunityIcons name="heart" color={color} size={24} />
           ),
         }}/>
 
@@ -51,18 +59,19 @@ function TabNavigator()
          options={{
           tabBarLabel: 'Search',
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="cloud-search" color={color} size={size} />
+            <MaterialCommunityIcons name="cloud-search" color={color} size={24} />
           ),
         }}/>
          <Tab.Screen name="Profile" component={ProfileInterface}  options={{
            tabBarLabel: 'Profile',
            activeBackgroundColor: "red",
            tabBarIcon: ({ color, size }) => (
-             <MaterialCommunityIcons name="face-profile" color={color} size={size} />
+             <MaterialCommunityIcons name="face-profile" color={color} size={24} />
            ),
          }}/>
        </Tab.Navigator>
     </NavigationContainer>
+    </SafeAreaView>
     </>
   )
 }
